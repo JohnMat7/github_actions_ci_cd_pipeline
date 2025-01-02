56eed908e629b4e3fa94fa255ee069d3747b41be
John Mathews <joffyjoffy8@gmail.com>
2025-01-02 14:47:26 +0530

    Update main.py

--- crud_api/main.py
+++ crud_api/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 @app.get("/welcome")
 def welcome():
-    return {"message": "Hi John Welcome Man!  (messaged added)"}
+    return {"message": "Hi John Welcome Man!  (messaged added and added again)"}
 
 
 
